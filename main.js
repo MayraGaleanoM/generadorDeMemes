4f82111e74e15783a4e07bf1a5084e154d3ddb0a
MayraGaleanoM <maygaleano00@gmail.com>
2022-11-07 21:21:38 -0300

    generadorDeMemes

---
+++ main.js
@@ -0,0 +1,11 @@
+//Variables
+const $ = () => document.querySelector(".buttonImagen"); document.querySelector("buttontext"); document.querySelector(".buttonlight");
+
+let btnImg = $(".buttonImagen");
+let btnTest =$(".buttonText");
+let btnlight =$(".buttonlight");
+
+//prueba verificacion consola
+console.log(btnImg);
+console.log(btnTest);
+console.log(btnlight);
